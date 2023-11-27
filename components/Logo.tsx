18d18117060b48bbc20fdb81e43ded5f4ff4e445
Alohayo <Garfield.bupt@gmail.com>
2023-11-27 15:43:25 +0800

    :art: format

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,21 +1,25 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import { useTheme } from 'next-themes'
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
-import DarkLogo from '@/data/logo-dark.svg'
-import LightLogo from '@/data/logo-light.svg'
+import DarkLogo from "@/data/logo-dark.svg";
+import LightLogo from "@/data/logo-light.svg";
 
 const ThemeSwitch = () => {
-  const [mounted, setMounted] = useState(false)
-  const { theme, resolvedTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, resolvedTheme } = useTheme();
 
-  useEffect(() => setMounted(true), [])
+  useEffect(() => setMounted(true), []);
 
   if (!mounted) {
-    return null
+    return null;
   }
-  return mounted && (theme === 'dark' || resolvedTheme === 'dark') ? <DarkLogo /> : <LightLogo />
-}
+  return mounted && (theme === "dark" || resolvedTheme === "dark") ? (
+    <DarkLogo />
+  ) : (
+    <LightLogo />
+  );
+};
 
-export default ThemeSwitch
+export default ThemeSwitch;
