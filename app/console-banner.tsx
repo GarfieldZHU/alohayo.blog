51a6e57be072fb6251472e558fc804e7be835c01
Alohayo <Garfield.bupt@gmail.com>
2024-07-18 15:23:16 +0800

    format: lint check

--- app/console-banner.tsx
+++ app/console-banner.tsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react'
 import { ThemeProvider } from 'next-themes'
 import siteMetadata from '@/data/siteMetadata'
 
-
 const CUSTOM_PAINITING = String.raw`
 ░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓██████▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████▓▓▓▓░░░░░░░░░░░░░░░░░░░░░░
 ░░░░░░░░░░░░░░░░░░░░▓▓██████████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▓▓██████████████▓▓░░░░░░░░░░░░░░░░
@@ -56,17 +55,18 @@ const DOMAIN_BANNER = String.raw`
                                                                                                                                                                                                                                                                                                                                                                                
 `
 
-const welcomeStyle = 'background-color: hotpink; color: white; font-style: w3-cursive; border: 2px solid; font-size: 3em;'
+const welcomeStyle = '
+    background-color: hotpink; color: white; font-style: w3-cursive; border: 2px solid; font-size: 3em;
+'
 
 /**
  * My custom banner printed to the console.
  */
 export function AlohaYoConsoleBanner() {
-
   useEffect(() => {
     console.log(CUSTOM_PAINITING)
     console.log(DOMAIN_BANNER)
-    console.log("%cWelcome to https://alohyo.me ~~~", welcomeStyle)
+    console.log('%cWelcome to https://alohyo.me ~~~', welcomeStyle)
   }, [])
 
   return <div className="alohayo_banner" />
