a2a5cd7295e45e26f1a6622f4d0bf5ada9753059
Garfield Zhu <Garfield.bupt@gmail.com>
2026-06-22 23:24:15 +0800

    chore: point blog game to updated fog build

--- app/game/GameLauncher.tsx
+++ app/game/GameLauncher.tsx
@@ -3,7 +3,7 @@
 import { FormEvent, useCallback, useEffect, useRef, useState } from 'react'
 import { useTheme } from 'next-themes'
 
-const GAME_MODULE_URL = 'https://garfieldzhu.github.io/alohayo-world/embed/bootstrap.js?v=f3957f9'
+const GAME_MODULE_URL = 'https://garfieldzhu.github.io/alohayo-world/embed/bootstrap.js?v=167abe8'
 const LOCALE_STORAGE_KEY = 'alohayo-world:locale'
 
 type LocaleCode = 'en' | 'zh-CN'
