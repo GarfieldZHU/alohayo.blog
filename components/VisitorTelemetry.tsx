0b2eb5fb77f3f2caa1fd5b9b96f7ad90621c7f0a
GarfieldZHU <garfield.bupt@gmail.com>
2026-07-29 15:34:14 +0800

    Adapt visitor badge to site themes

--- components/VisitorTelemetry.tsx
+++ components/VisitorTelemetry.tsx
@@ -1,12 +1,16 @@
 const counterUrl =
-  'https://hits.sh/alohayo.me.svg?view=total&style=flat-square&label=Total&color=5c9cf5&labelColor=6b7280'
+  'https://hits.sh/alohayo.me.svg?view=total&style=flat-square&label=Visitors&color=5c9cf5&labelColor=6b7280'
 
 export default function VisitorTelemetry() {
   return (
-    <div className="mb-2 flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
-      <span>Visitors</span>
+    <div className="mb-2 flex justify-center">
       <a href="https://hits.sh/alohayo.me/" aria-label="Open alohayo.me view statistics">
-        <img alt="Total views for alohayo.me" src={counterUrl} height={20} />
+        <img
+          alt="Total views for alohayo.me"
+          className="h-5 w-auto opacity-90 transition-opacity hover:opacity-100 dark:invert"
+          src={counterUrl}
+          height={20}
+        />
       </a>
     </div>
   )
